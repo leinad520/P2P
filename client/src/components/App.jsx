dcbc1c7b075416807a772e9e421f71e5c80fe46c
jimbosl1ce <jdhogerty@gmail.com>
2022-02-08 16:03:15 -0800

    Project Setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const App = (props) => (
-  <div>Hello World!</div>
+  <section>
+    {/* Austin */}
+    {/* Daniel */}
+    {/* Matt */}
+    {/* reviews */}
+  </section>
 );
 
 export default App;
