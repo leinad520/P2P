654c3df16b30a4df28100ab05a7d7e4d23d06617
changerbang <matthewtchang25@gmail.com>
2022-02-12 13:41:55 -0800

    Fixed merge conflicts

--- server/index.js
+++ server/index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-
-=======
 require('dotenv').config();
 const axios = require('axios');
->>>>>>> master
 const express = require('express');
-// const morgan = require('morgan');
 const app = express();
 const PORT = 3000 || process.env.PORT;
 const API_KEY = require('./config.js');
@@ -14,14 +9,6 @@ app.use(express.json());
 app.use(express.static('client/dist'));
 
 
-<<<<<<< HEAD
-// app.get()
-
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-})
-=======
 app.get('/products', (req, res) => {
   axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/', {
     headers: {
@@ -86,4 +73,3 @@ app.get('/products/:id/styles', (req, res) => {
   app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
   })
->>>>>>> master
