88fa350195f557b7fbc1127007278f44955ab4df
jimbosl1ce <jdhogerty@gmail.com>
2022-02-12 14:26:42 -0800

    Added everyones routes to server file

--- server/index.js
+++ server/index.js
@@ -19,6 +19,69 @@ app.use(express.static('client/dist'));
 
 axios.defaults.headers.common['Authorization'] = process.env.GITHUB_TOKEN;
 
+app.get('/products', (req, res) => {
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/', {
+    headers: {
+      Authorization: process.env.HR_TOKEN
+    }
+  })
+  .then(response => {res.send(response.data);})
+  .catch(err => res.send(err))
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${id}`, {
+    headers: {
+      Authorization: process.env.HR_TOKEN
+    }
+  })
+  .then(response => res.send(response.data))
+  .catch(err => res.send(err))
+})
+
+app.get('/products/:id/styles', (req, res) => {
+  const { id } = req.params
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${id}/styles`, {
+    headers: {
+      Authorization: process.env.HR_TOKEN
+    }
+  })
+  .then(response => res.send(response.data))
+  .catch(err => res.send(err))
+})
+
+
+  app.get("/api/:id/related", (req, res) => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${req.params.id}/related`, {
+      headers: {
+        Authorization: process.env.HR_TOKEN
+      }
+    })
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      res.send(err);
+    })
+  })
+
+  app.get("/api/:id", (req, res) => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${req.params.id}`, {
+      headers: {
+        Authorization: process.env.HR_TOKEN
+      }
+    })
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      res.send(err);
+    })
+  })
+
+
+
 
 // GET & SORT PRODUCTS
 app.get('/productreviews/:id/:sortType', (req, res) => {
