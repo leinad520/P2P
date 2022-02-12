9d6964900a258d718d18f3bb8a7fa1a6a3266fbd
jimbosl1ce <jdhogerty@gmail.com>
2022-02-12 13:46:06 -0800

    Server edits

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.static('client/dist'));
 
 
-axios.defaults.headers.common['Authorization'] = process.env.GITHUB_TOKEN;
+axios.defaults.headers.common['Authorization'] = process.env.HR_TOKEN;
 
 
 // GET & SORT PRODUCTS
