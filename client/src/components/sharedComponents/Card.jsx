ed12feb460fc7950fce951eb5f0aa299af812eee
jimbosl1ce <jdhogerty@gmail.com>
2022-02-14 16:20:54 -0800

    modified modalWindow to accept custom classes

--- client/src/components/sharedComponents/Card.jsx
+++ client/src/components/sharedComponents/Card.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 const Card = (props) => {
   const classes = `${props.className}` + ` review-container`;
-
-
   return (
     <div ref={props.forwardedRef} className={classes}>
       {props.children}
--- client/src/components/sharedComponents/modalComponent/Modal.jsx
+++ client/src/components/sharedComponents/modalComponent/Modal.jsx
@@ -2,21 +2,26 @@ import React, {useEffect, useRef} from 'react';
 
 const modalWindow = (props) => {
   const elInput = useRef(null);
+
   useEffect(() => {
     if (elInput.current !== null) {
-      console.log('working')
       elInput.current.style.left = '50%';
       elInput.current.style.display = 'flex';
     }
+  }, [props.onClose]);
 
-  }, [props.onClose])
+  const classes = `${props.className}` + ` jim-modal animate`;
+
+  // This modal takes a boolean to reveal
+  // This modal also takes a className prop, that
+  // passes itself onto here:
 
   if (!props.show) {
     return null;
   } else {
     return (
       <>
-      <div ref={elInput} className="jim-modal animate">
+      <div ref={elInput} className={classes}>
         {props.children}
         <button id="modal-close-btn" className="btn" onClick={props.onClose}>close</button>
       </div>
