b255fbe7fbead51bfa3c27292e2a6fef95b1bcd7
jimbosl1ce <jdhogerty@gmail.com>
2022-02-14 17:14:04 -0800

    ReactCouter commit

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,19 +6,20 @@ import ReviewSection from './ReviewComponent/ReviewSection.jsx';
 import { useParams } from 'react-router-dom';
 
 const App = (props) => {
-
   let productId = 42370;
   if (useParams().id) {
     productId = useParams().id
   }
 
-  return (<section>
-    {/* <Overview productId={productId}/> */}
+  return (
+  <section>
+    <Overview productId={productId}/> */}
     {/* Daniel */}
-    {/* <QA /> */}
-    {/* <RelatedProductsAndOutfit /> */}
+    <QA />
+    <RelatedProductsAndOutfit />
     <ReviewSection />
-  </section>)
+  </section>
+  );
 };
 
 export default App;
