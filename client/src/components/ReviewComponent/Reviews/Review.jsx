082cb7e77aed0520fee0846b11b4fa56f710e9ff
jimbosl1ce <jdhogerty@gmail.com>
2022-02-15 21:03:02 -0800

    Review.jsx refactored into smaller component folders

--- client/src/components/ReviewComponent/Reviews/Review.jsx
+++ client/src/components/ReviewComponent/Reviews/Review.jsx
@@ -1,10 +1,12 @@
 import React, {useState, useRef, useEffect} from 'react';
-import axios from 'axios';
 import moment from 'moment';
-import StarRating from '../../sharedComponents/starComponent/StarRating.jsx';
+import axios from 'axios';
+
 const checkmark = '../../../../static/checkmark.svg';
 import Card from '../../sharedComponents/Card.jsx';
 import ReviewPhotos from './ReviewPhotos.jsx';
+import ReviewDate from './ReviewDate.jsx';
+import ReviewLinks from './ReviewLinks.jsx';
 
 
 function chopText (text) {
@@ -22,73 +24,35 @@ function chopText (text) {
 };
 
 const Review = (props) => {
-  const [helpfulCount, setHelpfulCount] = useState(props.helpfulness);
   const elAnim = useRef(null);
-
   useEffect(() => {
     elAnim.current.style.transform = 'translateX(0%)'
   });
 
-  const momentDate = moment(props.date);
-
-  const reviewDate = {
-    year: momentDate.year(),
-    month: (momentDate.month() + 1),
-    day: (momentDate.day() +  + 1)
-  };
-
-  function onHelpfulClick(reviewId) {
-    axios({ method: 'put', url: 'http://localhost:3000/helpful', data: { reviewId }})
-      .then(success => {
-        setHelpfulCount(helpfulCount + 1);
-      })
-      .catch(err => console.log(err))
-  };
-
   return (
     <Card forwardedRef={elAnim} className={`animate`}>
       <div className="review-title-box">
-        <div className="review-star-date">
-          <StarRating rating={props.rating} />
-          <span>{props.username} - {reviewDate.month}/{reviewDate.day}/{reviewDate.year}</span>
-        </div>
+        <ReviewDate rating={props.rating} username={props.username} date={props.date}/>
         <h3>{props.title}</h3>
       </div>
-
       <div className="review-text">
         {chopText(props.text)}
       </div>
-
       {(props.photos.length > 0) && (
         <div className="review-photo-holder">
           {props.photos.map(photo => <ReviewPhotos key={photo.id} photo={photo}/>)}
         </div>
       )}
-
       {(props.recommend) &&
         <div className="review-recommend">
           <img src={checkmark}/> I recommend this product.
-        </div>
-      }
-
+        </div>}
       {(props.response) &&
         <div className="response">
           <h3>Response from seller:</h3>
           <div>{props.response}</div>
-        </div>
-        }
-
-      <div className="review-links">
-            <a
-              className={"helpful-link " + (helpfulCount > 0 ? '' : 'not-helpful-yet')}
-              onClick={() => onHelpfulClick(props.reviewId)}
-            >
-              Yes
-              <div className={'hidden ' + (helpfulCount > 0 ? 'helpful-count' : '')}>{helpfulCount}</div>
-            </a>
-          <a href="#" className="report">Report</a>
-      </div>
-      <div className="review-line" />
+        </div>}
+      <ReviewLinks helpfulness={props.helpfulness} reviewId={props.reviewId} />
     </Card>
   )
 }
