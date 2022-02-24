bd5e2fecd5915be0c578133c6380757993922374
Austin Beyaoju <Beyaoju@gmail.com>
2022-02-23 16:50:41 -0800

    Dark mode Across all products

--- client/src/components/QAComponents/QAListEntry.jsx
+++ client/src/components/QAComponents/QAListEntry.jsx
@@ -118,7 +118,7 @@ const QAListEntry = (props) => {
             <span>{'     '}</span>
             <span className="add-answer">
               <button id="add-answer-btn" onClick={e => showModal()}>
-                <u>Add Answer</u>
+                <u className='add-button-text'>Add Answer</u>
               </button>
               <div>
               <ModalWindow id="matt-modal" onClose={showModal} show={show}>
